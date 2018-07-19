9633baa66402acb9e1ee35064d38dff1902f85fd
Kuntal Dutta <kuntal.dt@gmail.com>
2018-07-19 11:11:47 +0530

    My Project setUp completed successfully

---
+++ shopingBeckend/src/main/java/net/kun/shopingBeckend/App.java
@@ -0,0 +1,13 @@
+package net.kun.shopingBeckend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
